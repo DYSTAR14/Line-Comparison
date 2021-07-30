78427b4a5054fd71d514992aa457d5dda1897fb0
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-30 12:23:47 +0530

    feat:I want to

--- src/com/blz/linecomparison/StartLineComparison.java
+++ src/com/blz/linecomparison/StartLineComparison.java
@@ -3,8 +3,9 @@ package com.blz.linecomparison;
 public class StartLineComparison {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Line Comparison Computation");
+		int x1=3,y1=7,x2=-2,y2=15;
+		int length=(int) ((Math.pow((x2-x1),2) + (Math.pow((y2-y1),2))));
+		System.out.println(Math.sqrt(length));
 	}
 
 }
