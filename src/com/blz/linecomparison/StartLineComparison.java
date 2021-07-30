f92efd03341cd149b35f3461bbb426f6b2f0eb91
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-30 12:26:18 +0530

    feat:Calculate Co-ordinates length

--- src/com/blz/linecomparison/StartLineComparison.java
+++ src/com/blz/linecomparison/StartLineComparison.java
@@ -3,7 +3,8 @@ package com.blz.linecomparison;
 public class StartLineComparison {
 
 	public static void main(String[] args) {
-		int x1=3,y1=7,x2=-2,y2=15;
+		int x1=3,y1=7;
+		int x2=-2,y2=15;
 		int length=(int) ((Math.pow((x2-x1),2) + (Math.pow((y2-y1),2))));
 		System.out.println(Math.sqrt(length));
 	}
