22acb90fbbf612a4eb237d1562e2d1daf56ac47b
DYSTAR14 <ydeepak141097@gmail.com>
2021-07-30 12:10:04 +0530

    ADD:Add files and Welcome to Line Comparison

---
+++ src/com/blz/linecomparison/StartLineComparison.java
@@ -0,0 +1,10 @@
+package com.blz.linecomparison;
+
+public class StartLineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+
+}
